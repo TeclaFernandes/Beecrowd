7fa7e3e31a59262c363db3b3936f9226f8b503b1
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2026-07-05 15:13:03 -0300

    feat: Exercicio 1478
    Matriz Quadrada 2

---
+++ Matriz quadrada 2.py
@@ -0,0 +1,11 @@
+while True:
+    n = int(input())
+    if n == 0:
+        break
+
+    for i in range(n):
+        linha = []
+        for j in range(n):
+            linha.append(f"{abs(i - j) + 1:3d}")
+        print(" ".join(linha))
+    print()
